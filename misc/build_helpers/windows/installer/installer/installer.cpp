3078bd3a30f883bab0e01f9a1d9467738f06f187
Daira Hopwood <daira@jacaranda.org>
2014-10-21 21:11:56 +0100

    Make a start on the Windows installer.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

---
+++ misc/build_helpers/windows/installer/installer/installer.cpp
@@ -0,0 +1,14 @@
+// installer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+	printf("hello");
+	return 0;
+}
+
